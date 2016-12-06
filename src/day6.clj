efe45b4dbb116b3ee1f9e28e759dbc5ca4d8aa87
Michiel Borkent <michielborkent@gmail.com>
2016-12-06 22:56:48 +0100

    Use min-key and max-key instead of sorting

--- src/day6.clj
+++ src/day6.clj
@@ -2,22 +2,25 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(def input (-> "day6.txt"
-               io/resource
-               slurp
-               str/trim
-               str/split-lines))
+(def rows
+  (with-open [rdr (-> "day6.txt"
+                      io/resource
+                      io/reader)]
+    (into [] (line-seq rdr))))
 
-(def columns (apply map vector input))
+(def columns
+  (apply map vector lines))
 
 ;; first answer
 (map #(->> %
            frequencies
-           (sort-by val >)
-           ffirst) columns) ;;=> (\u \m \e \j \z \g \d \w)
+           (apply max-key val)
+           first)
+     columns) ;;=> (\u \m \e \j \z \g \d \w)
 
 ;; second answer
 (map #(->> %
            frequencies
-           (sort-by val)
-           ffirst) columns) ;;=> (\a \o \v \u \e \a \k \v)
+           (apply min-key val)
+           first)
+     columns) ;;=> (\a \o \v \u \e \a \k \v)
